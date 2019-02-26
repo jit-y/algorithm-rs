6e70793d362dec8faf2c0fc82789b8ef49c3dfc4
YUJI takahashi <jit.y.31@gmail.com>
2019-02-26 22:36:03 +0900

    Add fibonacci::fib_to_vec

--- src/fibonacci.rs
+++ src/fibonacci.rs
@@ -6,6 +6,16 @@ pub fn fib(num: u32) -> u32 {
     }
 }
 
+pub fn fib_to_vec(num: i32) -> Vec<u32> {
+    let mut vec = Vec::with_capacity((num + 1) as usize);
+
+    for val in 0..=num {
+        vec.push(fib(val as u32));
+    }
+
+    vec
+}
+
 #[cfg(test)]
 mod tests {
     use crate::fibonacci;
@@ -16,5 +26,10 @@ mod tests {
         assert_eq!(1, fibonacci::fib(2));
         assert_eq!(2, fibonacci::fib(3));
         assert_eq!(3, fibonacci::fib(4));
+    }
+
+    #[test]
+    fn test_fib_to_vec() {
+        assert_eq!(vec![0, 1, 1, 2, 3], fibonacci::fib_to_vec(4));
     }
 }
